c1fa813671a211c863a6760d545864dbe19df3c7
KEISS <kss5122@naver.com>
2025-04-04 16:29:38 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/highfive/oliveyoung/OliveyoungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highfive.oliveyoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveyoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
