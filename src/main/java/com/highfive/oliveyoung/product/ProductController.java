3816f0b9b47b24851770c2b7d955915312dd1d9b
KEISS <86196038+ImKEISS@users.noreply.github.com>
2025-04-14 22:11:18 +0900

    feat: CORS 설정 추가
    - 모든 사이트 허용

--- src/main/java/com/highfive/oliveyoung/product/ProductController.java
+++ src/main/java/com/highfive/oliveyoung/product/ProductController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ProductController {
 
--- src/main/java/com/highfive/oliveyoung/stock/StockController.java
+++ src/main/java/com/highfive/oliveyoung/stock/StockController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/stocks")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class StockController {
 
